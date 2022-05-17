f30b3167741ff5801e73b4ad6eed3fa9f88550d9
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-17 08:24:26 -0300

    NextJS project setUp

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { Header } from '../components/Header'
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Insta App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
