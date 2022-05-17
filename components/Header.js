ec21715a3cae8d0bcb94e623322e549471a29347
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-17 09:05:34 -0300

    Header comp. in progress

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,38 @@
-import React from 'react'
-
+import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline'
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <main>
+      <section className="flex max-w-6xl justify-between">
+        <div
+          className="relative hidden h-24 w-24 lg:inline-grid"
+          cursor-pointer
+        >
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div>
+          <div></div>
+          <input type="text" placeholder="Search" />
+        </div>
+      </section>
+    </main>
+  )
 }
