525f8db22bc8ae38380fb5be39c4f5f5d28b3a3c
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-20 09:37:55 -0300

    NextAuth packg error, awaiting for update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,5 @@
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
-import GithubProvider from 'next-auth/providers/github'
 
 export default NextAuth({
   providers: [
@@ -12,23 +8,15 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      server: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // Passwordless / email sign in
-    EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: 'NextAuth.js <no-reply@example.com>',
-    }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
+    // // Passwordless / email sign in
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: 'NextAuth.js <no-reply@example.com>',
+    // }),
   ],
   pages: {
     signIn: '/auth/login',
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,27 @@
-function login() {
-  return <div>login</div>
+import { getProviders, signIn } from 'next-auth/react'
+
+function login({ providers }) {
+  console.log('PROVIDErs', providers)
+  return (
+    <>
+      {providers !== null &&
+        Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Sign In with {provider.name}
+            </button>
+          </div>
+        ))}
+    </>
+  )
+}
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: {
+      providers,
+    },
+  }
 }
 
 export default login
