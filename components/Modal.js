7945d2a527dd119f2b35187e3118e583c6c97a7a
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-23 11:55:40 -0300

    Modal in pg

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modal'
+import { Dialog, Transition } from '@headlessui/react'
 
 function Modal() {
-  return <div>Modal</div>
+  const [open, setOpen] = useRecoilState(modalState)
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={setOpen}
+      >
+        <div className="flex min-h-[800px] items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:min-h-screen sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterForm="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opaticy-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+          {/* Trick browser into center modal */}
+          <span className="hidden sm:inline-block sm:h-screen sm:align-middle">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterForm="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opaticy-0"
+          >
+            <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8">
+              <div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    disabled={!selectedFile}
+                    className="inline-flex w-full justify-center"
+                    onClick={uploadPost}
+                  >
+                    {loading ? 'Uploading... ' : 'Upload Post'}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
 }
-
 export default Modal
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
 import Modal from '../components/Modal'
+
 const Home = () => {
   return (
     <div className="h-screen overflow-y-scroll bg-gray-50 scrollbar-hide">
