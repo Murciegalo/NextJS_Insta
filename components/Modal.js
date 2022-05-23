a3100b3ec7eb74726f77178ad73dc3507fd34bcc
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-23 16:56:43 -0300

    Modal comp. UI & Hooks set, Firebase to be completed

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,30 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modal'
 import { Dialog, Transition } from '@headlessui/react'
+import { CameraIcon } from '@heroicons/react/outline'
 
 function Modal() {
   const [open, setOpen] = useRecoilState(modalState)
+  const filePickerRef = useRef(null)
+  const captionRef = useRef(null)
+  const [loading, setLoading] = useState(false)
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const uploadPost = async () => {
+    if (loading) return
+    setLoading(true)
+  }
+  const addImgToPost = (e) => {
+    const reader = new FileReader()
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0])
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result)
+    }
+  }
+
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -37,16 +57,60 @@ function Modal() {
             leaveFrom="opacity-100"
             leaveTo="opaticy-0"
           >
-            <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8">
+            <div className="sn:align-middle inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
               <div>
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    className="w-full cursor-pointer object-contain"
+                    onClick={() => setSelectedFile(null)}
+                    alt=""
+                  />
+                ) : (
+                  <div
+                    className="mx-auto flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-red-100"
+                    onClick={() => filePickerRef.current.click()}
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+                  </div>
+                  <div>
+                    <input
+                      ref={filePickerRef}
+                      type="file"
+                      hidden
+                      onChange={addImgToPost}
+                    />
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      className="w-full border-none text-center focus:ring-0"
+                      type="text"
+                      ref={captionRef}
+                      placeholder="Please enter a caption..."
+                    />
+                  </div>
+                </div>
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    // disabled={!selectedFile}
-                    className="inline-flex w-full justify-center"
+                    disabled={!selectedFile}
+                    className="hover:disabled-bg-gray-300 inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 sm:text-sm"
                     // onClick={uploadPost}
                   >
-                    Upload
+                    Upload Post
                   </button>
                 </div>
               </div>
